0c34e153d9e018193290c42a90b6747fb0cbb673
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2018-04-28 10:17:41 -0400

    fully conv network

--- models/text_objseg_model.py
+++ models/text_objseg_model.py
@@ -57,6 +57,39 @@ def text_objseg_cls(text_seq_batch, imcrop_batch, num_vocab, embed_dim,
 
     return mlp_l2
 
+def text_objseg_full_conv(text_seq_batch, imcrop_batch, num_vocab, embed_dim,
+    lstm_dim, mlp_hidden_dims, vgg_dropout, mlp_dropout):
+
+    # Language feature (LSTM hidden state)
+    feat_lang = lstm_net.lstm_net(text_seq_batch, num_vocab, embed_dim, lstm_dim)
+
+    # Local image feature
+    feat_vis = vgg_net.vgg_fc8_full_conv(imcrop_batch, 'vgg_local',
+        apply_dropout=vgg_dropout)
+
+    # Reshape and tile LSTM top
+    featmap_H, featmap_W = feat_vis.get_shape().as_list()[1:3]
+    N, D_text = feat_lang.get_shape().as_list()
+    feat_lang = tf.tile(tf.reshape(feat_lang, [N, 1, 1, D_text]),
+        [1, featmap_H, featmap_W, 1])
+
+    # L2-normalize the features (except for spatial_batch)
+    # and concatenate them along axis 3 (channel dimension)
+    spatial_batch = tf.convert_to_tensor(generate_spatial_batch(N, featmap_H, featmap_W))
+    feat_all = tf.concat(axis=3, values=[tf.nn.l2_normalize(feat_lang, 3),
+                             tf.nn.l2_normalize(feat_vis, 3),
+                             spatial_batch])
+
+    # MLP Classifier over concatenate feature
+    with tf.variable_scope('classifier'):
+        mlp_l1 = conv_relu('mlp_l1', feat_all, kernel_size=1, stride=1,
+            output_dim=mlp_hidden_dims)
+        if mlp_dropout: mlp_l1 = drop(mlp_l1, 0.5)
+        mlp_l2 = fc('mlp_l2', mlp_l1, output_dim=1)
+        # add third layer potentially
+
+    return mlp_l2
+
     
 def text_objseg_full_conv(text_seq_batch, imcrop_batch, num_vocab, embed_dim,
     lstm_dim, mlp_hidden_dims, vgg_dropout, mlp_dropout):
