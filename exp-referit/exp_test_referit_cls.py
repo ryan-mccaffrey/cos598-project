301e6488cd2efa00bf7ff6bc497bc4f33b595280
Ryan McCaffrey <ryan.mccaffrey.42@gmail.com>
2018-04-28 18:40:28 -0400

    v1 of the testing file for model

--- exp-referit/exp_test_referit_cls.py
+++ exp-referit/exp_test_referit_cls.py
@@ -8,6 +8,8 @@ import tensorflow as tf
 import json
 import timeit
 
+from random import randomint, shuffle
+from collections import defaultdict
 from models import vgg_net, lstm_net, processing_tools
 from util.cnn import fc_layer as fc
 from util.cnn import fc_relu_layer as fc_relu
@@ -23,11 +25,14 @@ imcrop_file = './exp-referit/data/referit_imcrop.json'
 imsize_file = './exp-referit/data/referit_imsize.json'
 vocab_file = './exp-referit/data/vocabulary_referit.txt'
 
-pretrained_model = './exp-referit/tfmodel/referit_fc8_cls_iter_25000.tfmodel'
+# Note: i believe fully trained model is iter_0.tfmodel
+pretrained_model = './exp-referit/tfmodel/referit_fc8_cls_iter_%d.tfmodel'
 
 # Model Params
 T = 20
-N = 100
+N = 10
+input_H = 224
+input_W = 224
 num_vocab = 8803
 embed_dim = 1000
 lstm_dim = 1000
@@ -46,30 +51,15 @@ nms_thresh = 0.3
 ################################################################################
 
 # Inputs
-text_seq_batch = tf.placeholder(tf.int32, [T, 1])  # one batch per sentence
-imcrop_batch = tf.placeholder(tf.float32, [N, 224, 224, 3])
-lstm_top_batch = tf.placeholder(tf.float32, [N, D_text])
-fc8_crop_batch = tf.placeholder(tf.float32, [N, D_im])
-spatial_batch = tf.placeholder(tf.float32, [N, 8])
-
-# Language feature (LSTM hidden state)
-lstm_top = lstm_net.lstm_net(text_seq_batch, num_vocab, embed_dim, lstm_dim)
-
-# Local image feature
-fc8_crop = vgg_net.vgg_fc8(imcrop_batch, 'vgg_local', apply_dropout=False)
-
-# L2-normalize the features (except for spatial_batch)
-# and concatenate them along axis 1 (feature dimension)
-feat_all = tf.concat(axis=1, values=[tf.nn.l2_normalize(lstm_top_batch, 1),
-                         tf.nn.l2_normalize(fc8_crop_batch, 1),
-                         spatial_batch])
+text_seq_batch = tf.placeholder(tf.int32, [T, N]) 
+imcrop_batch = tf.placeholder(tf.float32, [N, input_H, input_W, 3])
+label_batch = tf.placeholder(tf.float32, [N, 1])
 
 # Outputs
-# MLP Classifier over concatenate feature
-with tf.variable_scope('classifier'):
-    mlp_l1 = fc_relu('mlp_l1', feat_all, output_dim=mlp_hidden_dims)
-    mlp_l2 = fc('mlp_l2', mlp_l1, output_dim=1)
-scores = mlp_l2
+scores = segmodel.text_objseg_cls(text_seq_batch, imcrop_batch, 
+    num_vocab, embed_dim, lstm_dim, mlp_hidden_dims,
+    vgg_dropout=vgg_dropout, mlp_dropout=mlp_dropout)
+
 
 # Load pretrained model
 variable_name_mapping= None
@@ -84,108 +74,121 @@ if tf.__version__.split('.')[0] == '1':
 
 snapshot_restorer = tf.train.Saver(variable_name_mapping)
 sess = tf.Session()
-snapshot_saver.restore(sess, pretrained_model)
+# restores the iter_0.tfmodel
+snapshot_saver.restore(sess, pretrained_model % 0)
 
 ################################################################################
-# Load annotations and bounding box proposals
+# Load annotations
 ################################################################################
 
 query_dict = json.load(open(query_file))
-bbox_dict = json.load(open(bbox_file))
 imcrop_dict = json.load(open(imcrop_file))
 imsize_dict = json.load(open(imsize_file))
 imlist = list({name.split('_', 1)[0] + '.jpg' for name in query_dict})
 vocab_dict = text_processing.load_vocab_dict_from_file(vocab_file)
 
-# Object proposals
-bbox_proposal_dict = {}
-for imname in imlist:
-    bboxes = np.loadtxt(bbox_proposal_dir + imname[:-4] + '.txt').astype(int).reshape((-1, 4))
-    bbox_proposal_dict[imname] = bboxes
-
 ################################################################################
 # Flatten the annotations
 ################################################################################
+
+# counts the number of positive examples each imname associated with
+imexample_count = defaultdict(int)
 
 flat_query_dict = {imname: [] for imname in imlist}
 for imname in imlist:
     this_imcrop_names = imcrop_dict[imname]
     for imcrop_name in this_imcrop_names:
-        gt_bbox = bbox_dict[imcrop_name]
         if imcrop_name not in query_dict:
             continue
         this_descriptions = query_dict[imcrop_name]
         for description in this_descriptions:
-            flat_query_dict[imname].append((imcrop_name, gt_bbox, description))
+            flat_query_dict[imname].append((description, 1))
+            imexample_count[imname] += 1
+
+# add false examples to the flat_query_dict
+for i, imname in enumerate(imexample_count):
+    this_imcrop_names = imcrop_dict[imname]
+    
+    j = 0
+    while j < len(imexample_count[imname]):
+        # choose random image index in imlist
+        rand_img_idx = i
+        while rand_img_idx == i:
+            rand_img_idx = randint(0, len(imlist)-1)
+        rand_imname = imlist[rand_img_idx]
+        rand_imcrop_names = imcrop_dict[rand_imname]
+
+        # choose random description by shuffling possible descriptions,
+        # checking if each is in test set. If none are, must try choosing
+        # new random index
+        shuffle(rand_imcrop_names)
+        rand_description = None
+        for imcrop_name in rand_imcrop_names:
+            if imcrop_name in query_dict:
+                # always choose first caption, since most imcrops only have
+                # one caption (and too bad for the other ones)
+                rand_description = query_dict[imcrop_name][0]
+                break
+
+        # checks that we found a caption. If no good caption to choose from, start
+        # loop over by choosing new random image
+        if rand_description is not None:
+            flat_query_dict[imname].append((rand_description, 0))
+            j += 1
 
 ################################################################################
 # Testing
 ################################################################################
 
-eval_bbox_num_list = [1, 10, 100]
-bbox_correct = np.zeros(len(eval_bbox_num_list), dtype=np.int32)
-bbox_total = 0
+# TODO: run through the model
+# At this point, have a dictionary called flat_query_dict which is
+# key: image name, value: tuple of (description, label)
+# Note that each image name has values that have real descriptions with label 1,
+# and an equal number of "bad" descriptions with label 0.
+
+correct_predictions = 0
+total_predictions = 0
 
 # Pre-allocate arrays
-imcrop_val = np.zeros((N, 224, 224, 3), dtype=np.float32)
-spatial_val = np.zeros((N, 8), dtype=np.float32)
-text_seq_val = np.zeros((T, 1), dtype=np.int32)
-lstm_top_val = np.zeros((N, D_text))
+imcrop_val = np.zeros((N, input_H, input_W, 3), dtype=np.float32)
+text_seq_val = np.zeros((T, N), dtype=np.int32)
+
 
 num_im = len(imlist)
 for n_im in range(num_im):
     print('testing image %d / %d' % (n_im, num_im))
     imname = imlist[n_im]
     imsize = imsize_dict[imname]
-    bbox_proposals = bbox_proposal_dict[imname]
-    num_proposal = bbox_proposals.shape[0]
-    assert(N >= num_proposal)
+    # bbox_proposals = bbox_proposal_dict[imname]
+    # num_proposal = bbox_proposals.shape[0]
+    # assert(N >= num_proposal)
 
     # Extract visual features from all proposals
+    # TODO: Process image before testing? Must be consistent with how training
     im = skimage.io.imread(image_dir + imname)
-    if im.ndim == 2:
-        im = np.tile(im[:, :, np.newaxis], (1, 1, 3))
-    imcrop_val[:num_proposal, ...] = im_processing.crop_bboxes_subtract_mean(
-        im, bbox_proposals, 224, vgg_net.channel_mean)
-    fc8_crop_val = sess.run(fc8_crop, feed_dict={imcrop_batch:imcrop_val})
+    processed_im = skimage.img_as_ubyte(im_processing.resize_and_pad(im, input_H, input_W))
+    if processed_im.ndim == 2:
+        processed_im = np.tile(processed_im[:, :, np.newaxis], (1, 1, 3))
+    
 
-    # Extract bounding box features from proposals
-    spatial_val[:num_proposal, ...] = \
-        processing_tools.spatial_feature_from_bbox(bbox_proposals, imsize)
+    imcrop_val[...] = processed_im.astype(np.float32) - segmodel.vgg_net.channel_mean
 
     # Extract textual features from sentences
-    for imcrop_name, gt_bbox, description in flat_query_dict[imname]:
-        proposal_IoUs = eval_tools.compute_bbox_iou(bbox_proposals, gt_bbox)
-
+    for description, im_label in flat_query_dict[imname]:
         # Extract language feature
         text_seq_val[:, 0] = text_processing.preprocess_sentence(description, vocab_dict, T)
-        lstm_top_val[...] = sess.run(lstm_top, feed_dict={text_seq_batch:text_seq_val})
+        
+        # TODO: ensure running through model correctly, this is the prediction step
+        scores_val = sess.run(scores, feed_dict={
+            text_seq_batch  : text_seq_val,
+            imcrop_batch    : imcrop_val
+        })
+        scores_val = np.squeeze(scores_val)
 
-        # Compute scores per proposal
-        scores_val = sess.run(scores,
-            feed_dict={
-                lstm_top_batch:lstm_top_val,
-                fc8_crop_batch:fc8_crop_val,
-                spatial_batch:spatial_val
-            })
-        scores_val = scores_val[:num_proposal, ...].reshape(-1)
-
-        # Sort the scores for the proposals
-        if use_nms:
-            top_ids = eval_tools.nms(proposal.astype(np.float32), scores_val, nms_thresh)
-        else:
-            top_ids = np.argsort(scores_val)[::-1]
-
-        # Evaluate on bounding boxes
-        for n_eval_num in range(len(eval_bbox_num_list)):
-            eval_bbox_num = eval_bbox_num_list[n_eval_num]
-            bbox_correct[n_eval_num] += \
-                np.any(proposal_IoUs[top_ids[:eval_bbox_num]] >= correct_iou_thresh)
-        bbox_total += 1
+        # count if correct
+        correct_predictions += (scores_val[0] == im_label)
+        total_predictions += 1
 
 print('Final results on the whole test set')
-result_str = ''
-for n_eval_num in range(len(eval_bbox_num_list)):
-    result_str += 'recall@%s = %f\n' % \
-        (str(eval_bbox_num_list[n_eval_num]), bbox_correct[n_eval_num]/bbox_total)
+result_str = 'recall = %f\n'.format(float(correct_predictions)/total_predictions)
 print(result_str)
