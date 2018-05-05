6c3f051cf65a144bdfb0b47fb2a27e58a0784556
Yannis Karakozis <I-C-Karakozis@users.noreply.github.com>
2018-05-05 13:25:33 -0400

    incorporating glove embedding analysis

--- exp-referit/vocab_lstm_pca.py
+++ exp-referit/vocab_lstm_pca.py
@@ -9,6 +9,7 @@ import argparse
 from matplotlib import pyplot as plt
 from sklearn.manifold import TSNE
 from scipy import spatial
+from tensorflow.contrib import learn
 
 from models import text_objseg_model as segmodel
 from models import vgg_net, lstm_net, processing_tools
@@ -20,9 +21,17 @@ from util import text_processing
 # Parameters
 ################################################################################
 
-vocab_file = './exp-referit/data/vocabulary_referit.txt'
-pretrained_model = './exp-referit/tfmodel/referit_fc8_det_iter_25000.tfmodel'
 plot_dir = 'tSNE'
+vector_count = 500
+
+# glove file
+glove = './exp-referit/data/glove.6B.50d.txt'
+
+# load dataset vocabulary
+vocab_file = './exp-referit/data/vocabulary_referit.txt'
+vocab_dict = text_processing.load_vocab_dict_from_file(vocab_file)
+words = list(vocab_dict.keys())
+print("Unique words in vocabulary:", len(words))
 
 # Model Params
 T = 1
@@ -30,13 +39,13 @@ N = 1
 num_vocab = 8803
 embed_dim = 1000
 lstm_dim = 1000
-mlp_hidden_dims = 500
-
-D_im = 1000
 D_text = lstm_dim
 
-# np.seterr(divide='ignore', invalid='ignore')
+################################################################################
+# Helper Functions
+################################################################################
 
+# plot lower dimensionality embedding
 def plot_with_labels(low_dim_embs, labels, filename):
   assert low_dim_embs.shape[0] >= len(labels), "More labels than embeddings"
   plt.figure(figsize=(18, 18))  #in inches
@@ -52,45 +61,40 @@ def plot_with_labels(low_dim_embs, labels, filename):
 
   plt.savefig(os.path.join(plot_dir,filename))
 
-def main(args):
+# load GloVe embedding matrix
+def loadGloVe(filename):
+    vocab = []
+    embd = []
+    file = open(filename,'r', encoding="utf8")
+    for line in file.readlines():
+        row = line.strip().split(' ')
+        vocab.append(row[0])
+        embd.append(row[1:])
+    print('Loaded GloVe!')
+    file.close()
+    return vocab,embd
+
+# generate vectors and words from learnt embedding
+def vectorizeLearntEmbd(args):
     if args.checkpoint == '':
-        ################################################################################
-        # Load evaluation network
-        ################################################################################
+        ### Load LSTM ###
+
+        # Determine network:
+        if args.savefile == "det":
+            pretrained_model = './exp-referit/tfmodel/referit_fc8_det_iter_25000.tfmodel'
+        else:
+            pretrained_model = './exp-referit/tfmodel/model_crop.tfmodel'
 
         # Inputs
         text_seq_batch = tf.placeholder(tf.int32, [T, N])
-        imcrop_batch = tf.placeholder(tf.float32, [N, 224, 224, 3])
         lstm_top_batch = tf.placeholder(tf.float32, [N, D_text])
-        fc8_crop_batch = tf.placeholder(tf.float32, [N, D_im])
-        spatial_batch = tf.placeholder(tf.float32, [N, 8])
 
         # Language feature (LSTM hidden state)
-        lstm_top = lstm_net.lstm_net_tsne(text_seq_batch, num_vocab, embed_dim, lstm_dim)
-
-        # Local image feature
-        fc8_crop = vgg_net.vgg_fc8(imcrop_batch, 'vgg_local', apply_dropout=False)
-
-        # L2-normalize the features (except for spatial_batch)
-        # and concatenate them along axis 1 (feature dimension)
-        if args.savefile == "det":
-            feat_all = tf.concat(axis=1, values=[tf.nn.l2_normalize(lstm_top_batch, 1),
-                                     tf.nn.l2_normalize(fc8_crop_batch, 1),
-                                     spatial_batch])
-        else:
-            feat_all = tf.concat(axis=1, values=[tf.nn.l2_normalize(lstm_top_batch, 1),
-                                 tf.nn.l2_normalize(fc8_crop_batch, 1)])
-
-        # Outputs
-        # MLP Classifier over concatenate feature
-        with tf.variable_scope('classifier'):
-            mlp_l1 = fc_relu('mlp_l1', feat_all, output_dim=mlp_hidden_dims)
-            mlp_l2 = fc('mlp_l2', mlp_l1, output_dim=1)
-        scores = mlp_l2
+        lstm_top = lstm_net.lstm_net(text_seq_batch, num_vocab, embed_dim, lstm_dim)
 
         # Load pretrained model
+        variable_name_mapping= None
         if args.savefile == "det":
-            variable_name_mapping= None
             if tf.__version__.split('.')[0] == '1':
                 variable_name_mapping = {
                     v.op.name.replace(
@@ -99,35 +103,21 @@ def main(args):
                         'rnn/multi_rnn_cell/cell_0/basic_lstm_cell/bias',
                         'RNN/MultiRNNCell/Cell0/BasicLSTMCell/Linear/Bias'): v
                     for v in tf.global_variables()}
-            snapshot_restorer = tf.train.Saver(variable_name_mapping)
-        else:
-            snapshot_restorer = tf.train.Saver(None)
-
+        snapshot_restorer = tf.train.Saver(variable_name_mapping)
         sess = tf.Session()
-        snapshot_restorer.restore(sess, pretrained_model)
+        snapshot_restorer.restore(sess, pretrained_model)     
 
-        ################################################################################
-        # Load vocabulary
-        ################################################################################
-
-        vocab_dict = text_processing.load_vocab_dict_from_file(vocab_file)
-
-        ################################################################################
-        # Testing
-        ################################################################################
+        ### Generate vectors ###
 
         # Pre-allocate arrays
         text_seq_val = np.zeros((T, N), dtype=np.int32)
         lstm_top_val = np.zeros((N, D_text))
         embd_val = np.zeros((N, embed_dim))
-        words = list(vocab_dict.keys())
         lstm_vectors = list()
         embd_vectors = list()
 
-        print("Unique words in vocabulary:", len(words))
-        count = 0
-
         # extract LSTM feature lstm_vectors per word
+        count = 0
         for word in words: 
             count += 1
             if count % 100 == 0: print("%d out of %d words processed" % (count, len(words)))
@@ -137,13 +127,13 @@ def main(args):
             text_seq_val[:, 0] = text_seq
 
             # Extract LSTM language feature and word embedding vector
-            lstm_top_val[...], embd_val[...]  = sess.run(lstm_top, feed_dict={text_seq_batch:text_seq_val})
+            lstm_top_val[...]  = sess.run(lstm_top, feed_dict={text_seq_batch:text_seq_val})
             temp = np.squeeze(np.transpose(lstm_top_val))
             lstm_vectors.append(temp)
             temp = np.squeeze(np.transpose(embd_val))
             embd_vectors.append(temp)
 
-            # if count == 100: break
+            if count == vector_count: break
 
         # Save TSNE lstm_vectors for easy recovery
         backup = args.savefile + "_TSNE_backup.npz"
@@ -153,39 +143,79 @@ def main(args):
     else:
         # Load saved lstm_vectors
         npzfile = np.load(os.path.join(plot_dir, args.checkpoint))
-        words = npzfile['words']
         lstm_vectors = npzfile['lstm_vectors']
-        embd_vectors = npzfile['embd_vectors'] 
+        embd_vectors = npzfile['embd_vectors']
 
+    return lstm_vectors, embd_vectors
+
+################################################################################
+# Main
+################################################################################
+
+def main(args):
+    if args.glove:
+        # load GloVe
+        vocab, embd = loadGloVe(glove)
+        embedding = np.asarray(embd)
+        print("Embedding dimensions:", np.shape(embedding))
+
+        embd_vectors = list()
+        adj_words = list()
+
+        # collect word embedding for every word
+        count = 0
+        for word in words: 
+            count += 1
+            if count % 100 == 0: print("%d out of %d words processed" % (count, len(words)))
+
+            if word in vocab:
+                embd_val = embd[vocab.index(word)]
+                embd_vectors.append(embd_val)
+                adj_words.append(word)
+            else: 
+                print(word)
+
+            if count == vector_count: break
+    else:
+        lstm_vectors, embd_vectors = vectorizeLearntEmbd(args)    
+    
     if args.k_nearest <= 0:
-        # Perform tSNE on LSTM vectors
-        X_embedded = TSNE(n_components=2).fit_transform(lstm_vectors)
-        plot_with_labels(X_embedded, words[:len(lstm_vectors)], args.savefile+"_TSNE_lstm.png")
+        if not args.glove:
+            # Perform tSNE on LSTM vectors
+            X_embedded = TSNE(n_components=2).fit_transform(lstm_vectors)
+            plot_with_labels(X_embedded, words[:len(lstm_vectors)], args.savefile+"_TSNE_lstm.png")
         
-        # Perform tSNE on word embeddings
-        X_embedded = TSNE(n_components=2).fit_transform(embd_vectors)
-        plot_with_labels(X_embedded, words[:len(embd_vectors)], args.savefile+"_TSNE_embd.png")
+            # Perform tSNE on word embeddings
+            X_embedded = TSNE(n_components=2).fit_transform(embd_vectors)
+            plot_with_labels(X_embedded, words[:len(embd_vectors)], args.savefile+"_TSNE_embd.png")
+
+        else: 
+            # Perform tSNE on word embeddings
+            X_embedded = TSNE(n_components=2).fit_transform(embd_vectors)
+            plot_with_labels(X_embedded, adj_words[:len(embd_vectors)], args.savefile+"_TSNE_embd_glove.png")
+
     else:
         # Setup KDTree algorithm for nearest neighbors
-        tree = spatial.KDTree(embd_vectors)
+        tree = spatial.KDTree(lstm_vectors)
         with open("testfile.txt","w") as file:
             file.write("")
 
         # Find k nearest neighbors for every word.
-        for vec_index in range(len(embd_vectors)):
-            dist, indxs = tree.query(embd_vectors[vec_index],k=args.k_nearest)
+        for vec_index in range(len(lstm_vectors)):
+            dist, indxs = tree.query(lstm_vectors[vec_index], k=args.k_nearest)
             results_str = words[vec_index] + ":"
             for i in indxs:
                 results_str = results_str + " " + words[i]  
             with open("testfile.txt","a") as file:
                 file.write(results_str + "\n")
+                file.write(str(sum(lstm_vectors[vec_index]-lstm_vectors[0])) + "\n")
 
 '''
 Sample tSNE execution: 
-python exp-referit/vocab_lstm_pca.py cls_crop -o cls_crop_SNE_backup.npz
+python exp-referit/vocab_lstm_pca.py det --custom -o det_tSNE_backup.npz
 
 Sample k-nearest execution:
-python exp-referit/vocab_lstm_pca.py cls_crop 5 -o cls_crop_SNE_backup.npz
+python exp-referit/vocab_lstm_pca.py cls_crop 5 --custom -o cls_crop_tSNE_backup.npz
 '''
 DESCRIPTION = """Language vector analysis suite: tSNE and k-nearest."""
 
@@ -193,6 +223,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=DESCRIPTION)
     parser.add_argument('savefile', help='Prefix of file to save the final plot in.') 
     parser.add_argument('k_nearest', type=int, nargs='?', default=-1)
+    parser.add_argument('--glove', dest='glove', action='store_true')
+    parser.add_argument('--custom', dest='glove', action='store_false')
     parser.add_argument('-o', '--checkpoint', nargs='?', default='', help='Checkpointed lstm_vectors npz file.')  
     args = parser.parse_args()
     main(args)
